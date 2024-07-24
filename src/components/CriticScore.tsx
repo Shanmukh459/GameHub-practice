957218000cdb8ae1e66e8a2c9b53a32f7e3c7b0a
Shanmukh459 <saikrrish459@gmail.com>
2024-07-24 12:00:33 -0700

    dplaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react"
+import React from "react"
+
+interface Props {
+  score: number
+}
+
+const CriticScore = ({ score }: Props) => {
+  const colorScheme = score > 75 ? "green" : score > 60 ? "yellow" : ""
+  return (
+    <Badge
+      colorScheme={colorScheme}
+      fontSize="14px"
+      paddingX={2}
+      borderRadius="4px"
+    >
+      {score}
+    </Badge>
+  )
+}
+
+export default CriticScore
