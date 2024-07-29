ec17e966de57aa5d47137c969bd30359f4518468
Shanmukh459 <saikrrish459@gmail.com>
2024-07-29 12:50:14 -0700

    adding emojies based on ratings

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames"
 import PlatformIcons from "./PlatformIcons"
 import CriticScore from "./CriticScore"
 import getCroppedImage from "../services/imageUrl"
+import Emoji from "./Emoji"
 
 interface Props {
   game: Game
@@ -19,7 +20,11 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name}
+
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   )
