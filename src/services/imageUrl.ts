3be665b1707897d19e708a2396d5f8d55632c7c1
Shanmukh459 <saikrrish459@gmail.com>
2024-07-29 11:44:11 -0700

    default image added for no image game cards

--- src/services/imageUrl.ts
+++ src/services/imageUrl.ts
@@ -1,4 +1,7 @@
+import defaultImage from "../assets/no-image-placeholder-6f3882e0.webp"
+
 const getCroppedImage = (url: string) => {
+  if (!url) return defaultImage
   const target = "media/"
   const index = url.indexOf(target) + target.length
   return url.slice(0, index) + "crop/600/400/" + url.slice(index)
